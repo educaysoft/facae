fa1e8549938663f4d803cfca9ef7b126762e12a6
Stain Francis <educaysoft@gmail.com>
2023-04-19 00:25:35 -0400

    inicio

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("https://educaysoft.org/sica/index.php/login/user_registration_show?idevento=338");
+header("location: https://educaysoft.org/sica/index.php/login/user_registration_show?idevento=338");
 
 ?>
