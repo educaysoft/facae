53047ce0bd5359c37f5b58ecb8df0f149b989fac
Stalin Francis <educaysoft@gmail.com>
2023-05-10 08:36:37 -0500

    face

---
+++ 2023/congresoutlvte-ejestematicos.php
@@ -0,0 +1,107 @@
+<div id="equipo" style="border-top: 2px solid green;    width: 90%; margin:20px auto; display: inline-block; ";
+<div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 20vh; display: inline-block; ">
+    
+
+
+<main>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+ <div class="row py-lg-5 text-center container">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Ejes temáticos </h1>
+      </div>
+    </div>
+
+
+
+ <div class="my-3 p-3 bg-body rounded shadow-sm">
+    <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#FFFFFF" dy=".3em">1</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Responsabilidad social empresarial, economía circular, economía de colores y procesos regenerativos.
+      </p>
+    </div>
+    <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">2</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Empresa pública y privada y su rol en el contexto actual.
+      </p>
+    </div>
+    <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">3</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Gestión innovadora y disruptiva en los negocios actuales.
+      </p>
+    </div>
+
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#33ffa9"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">4</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+ Comercio, contabilidad, banca y finanzas.
+      </p>
+    </div>
+
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #42534c"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">5</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Negocios internacionales e integración económica.
+      </p>
+    </div>
+
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #D673D7"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">6</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Tecnología y su relación con el crecimiento y el desarrollo económico.
+      </p>
+    </div>
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #C8BECO"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">7</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Movilidad y seguridad humana en el desarrollo económico.
+      </p>
+    </div>
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #FB525A"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">8</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Propuestas de nuevas carreras, tecnologías y programas de enseñanza en el campo ingenieril.
+      </p>
+    </div>
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #DBDD25"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">9</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Casos de estudio.
+      </p>
+    </div>
+
+    <small class="d-block text-end mt-3">
+      <a href="https://congresoutlvte.org/faci/congresoutlvte-tematicas.php">Todas las temáticas</a>
+    </small>
+  </div>
+
+  
+
+
+      
+    </div>
+  </div>
+
+</main>
+
+</div>
---
+++ 2023/congresoutlvte-slide.php
@@ -0,0 +1,103 @@
+<div id="equipo" style="border-top: 2px solid green;    width: 90%; margin:20px auto; display: inline-block;">
+<div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 100%; display: inline-block; ">
+
+
+<div style="float:left; width: 100%; font-size: 0.5wv;padding:0%">
+
+
+
+  <main>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+<div class="row py-lg-5 text-center container">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Presentación y participación al evento</h1>
+      </div>
+    </div>
+
+
+
+
+
+
+<!-- Carousel -->
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+  <!-- Indicators/dots -->
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="4"></button>
+  </div>
+  
+  <!-- The slideshow/carousel -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <a href="congresoutlvte-presentacion.php" class="stretched-link"><img src="images/slide/faci2.jpg" alt="Los Angeles" class="d-block" style="width:100%"></a>
+      <div class="carousel-caption">
+        <h3></h3>
+        <p></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/slide/DocentePonente.jpg" alt="Chicago" class="d-block" style="width:100%">
+      <div class="carousel-caption">
+        <h3 style="color:red">Docente ponente</h3>
+        <p> <a class="btn btn-primary my-2"  href="congresoutlvte-inscripcion.php"> Inscribete ya </a> </p>
+      </div> 
+    </div>
+    <div class="carousel-item">
+      <img src="images/slide/EstudiantePonente2.jpg" alt="New York" class="d-block" style="width:100%">
+      <div class="carousel-caption">
+        <h3 style="color:black">Estudiante ponente</h3>
+        <p><a class="btn btn-primary my-2"  href="congresoutlvte-inscripcion.php"> Inscribete ya</a></p>
+      </div>  
+    </div>
+
+<div class="carousel-item">
+      <img src="images/slide/EstudianteParticipante2.jpg" alt="New York" class="d-block" style="width:100%">
+      <div class="carousel-caption">
+        <h3>Estudiantes participantes</h3>
+        <p><a class="btn btn-primary my-2"  href="congresoutlvte-inscripcion.php"> Inscribete ya</a></p>
+      </div>  
+    </div>
+
+<div class="carousel-item">
+      <img src="images/slide/DocenteParticipante1.jpg" alt="New York" class="d-block" style="width:100%">
+      <div class="carousel-caption">
+        <h3>Docentes participantes</h3>
+        <p><a class="btn btn-primary my-2"  href="congresoutlvte-inscripcion.php"> Inscribete ya </a></p>
+      </div>  
+    </div>
+  </div>
+
+
+
+  <!-- Left and right controls/icons -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>
+
+<div class="container-fluid mt-3">
+  <h3></h3>
+  <p></p>
+</div>
+
+
+
+</div>
+</div>
+   
+		
+  </main>
+</div>
+</div>
+</div>
