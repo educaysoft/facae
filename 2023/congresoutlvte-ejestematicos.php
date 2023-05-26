7c70bd6fd6431db39108b615448042cddfa57884
Stain Francis <educaysoft@gmail.com>
2023-05-26 11:27:29 -0400

    mejoras

--- 2023/congresoutlvte-ejestematicos.php
+++ 2023/congresoutlvte-ejestematicos.php
@@ -21,72 +21,69 @@
       <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#FFFFFF" dy=".3em">1</text></svg>
 
       <p class="pb-3 mb-0 small lh-sm border-bottom">
-Responsabilidad social empresarial, economía circular, economía de colores y procesos regenerativos.<br>Coordinador: Ing. Carlos Rivas Vallejo, Mgs. 
+Responsabilidad social empresarial, economía circular, economía de colores y procesos regenerativos.<br><b>Coordinador:</b> Ing. Carlos Rivas Vallejo, Mgs. 
       </p>
     </div>
-    <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">2</text></svg>
+   
+<div class="d-flex text-muted pt-3">
+<svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">2</text></svg>
 
       <p class="pb-3 mb-0 small lh-sm border-bottom">
-Liderazgo, comunicación interna y Cultura Organizacional.<br> Ing. Romina Oyarvide Ibarra, Mgs.
-      </p>
-    </div>
-    <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">3</text></svg>
-
-      <p class="pb-3 mb-0 small lh-sm border-bottom">
-Gestión innovadora y disruptiva en los negocios actuales.
+Calidad de la Gestión Innovadora y descriptiva de los negocios.<br><b>Coordinadora:</b> Dra. Olga Quiñonez Cagua, PhD.
       </p>
     </div>
 
 
 <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#33ffa9"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">4</text></svg>
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #D673D7"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">3</text></svg>
 
       <p class="pb-3 mb-0 small lh-sm border-bottom">
- Rol de los escenarios actuales de la empresa pública y privada: Ing. Luis Canchingre Bone, Mgs.
+Tecnología y su relación con el crecimiento y el desarrollo económico.<br><b>Coordinador:</b> Ing. Cesar Godoy Rosero, Mgs.
+      </p>
+    </div>
+ 
+ <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #FB525A"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">4</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Economía del siglo XXI economía circular y en colores.<br><b>Coordinador:</b> Jorgeli Ayovi Caicedo, Mgs.
+      </p>
+    </div>   
+
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#33ffa9"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">5</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+ Rol de los escenarios actuales de la empresa pública y privada.<br><b>Coordinador:</b> Ing. Luis Canchingre Bone, Mgs.
       </p>
     </div>
 
 
-<div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #42534c"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">5</text></svg>
 
-      <p class="pb-3 mb-0 small lh-sm border-bottom">
-Calidad de la Gestión Innovadora y descriptiva de los negocios.<br> Dra. Olga Quiñonez Cagua, PhD.
+
+
+
+
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #DBDD25"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">6</text></svg>
+<p class="pb-3 mb-0 small lh-sm border-bottom">
+Movilidad y seguridad humana en el desarrollo económico.<br><b>Coordinador:</b> Ing. Patricio Arce Minda, Msg.
       </p>
+      
     </div>
 
 
-<div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #D673D7"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">6</text></svg>
 
-      <p class="pb-3 mb-0 small lh-sm border-bottom">
-Tecnología y su relación con el crecimiento y el desarrollo económico.<br>Coordinador: Ing. Cesar Godoy Rosero, Mgs.
-      </p>
-    </div>
+
+
 
 <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #C8BECO"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">7</text></svg>
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">7</text></svg>
 
       <p class="pb-3 mb-0 small lh-sm border-bottom">
-Movilidad y seguridad humana en el desarrollo económico.<br>Coordinador: Ing. Patricio Arce Minda, Msg.
-      </p>
-    </div>
-
-<div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #FB525A"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">8</text></svg>
-
-      <p class="pb-3 mb-0 small lh-sm border-bottom">
-Economía del siglo XXI economía circular y en colores.<br>Coordinador: Jorgeli Ayovi Caicedo, Mgs.
-      </p>
-    </div>
-
-<div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #DBDD25"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">9</text></svg>
-
-      <p class="pb-3 mb-0 small lh-sm border-bottom">
-Casos de estudio.
+Liderazgo, comunicación interna y Cultura Organizacional.<br><b>Coordinadora:</b> Ing. Romina Oyarvide Ibarra, Mgs.
       </p>
     </div>
 
