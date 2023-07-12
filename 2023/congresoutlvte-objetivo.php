73d52b3c5f5540edf33c0f2ea251090c10598bdb
Stain Francis <educaysoft@gmail.com>
2023-07-12 17:37:22 -0400

    mejoras

--- 2023/congresoutlvte-objetivo.php
+++ 2023/congresoutlvte-objetivo.php
@@ -1,6 +1,6 @@
 <div id="seccion1" style="border-top: 2px solid green;   width: 90%; margin:20px auto; display: inline-block; ">
 <div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 100%; display: inline-block; ">
-<div class="my-fixed-item w3-card-4" style="width: 100%; margin: 10px auto; background: rgba(218, 247,166  , 0.50); border: 1px solid black; padding:10px;  ">
+<div class="my-fixed-item w3-card-4" style="width: 100%; margin: 10px auto; background: rgba(218, 247,166  , 0.50); border-radius:5px; border: 1px solid black; padding:10px;  ">
 <p style="color: black; font-size:2vw; font-weight: bold;">Objetivo</p>
 <p style="text-align:justify; font-size: 1.5vw; margin:10px 0 10px 0;">Presentar a la comunidad científica internacional los avances y productos de investigación asociados a las Ciencias Administrativas y Económicas, a través de conferencias, ponencias y poster que contribuyan a la formación profesional".</p>
 
--- 2023/index.php
+++ 2023/index.php
@@ -5,7 +5,7 @@
 	<meta name="description" content=""IV Congreso Internacional Ciencias Administrativas y Económicas" />
 	<meta property="og:locale" content="es_EC" />
 	<meta property="og:type" content="website" />
-	<meta property="og:title" content="IV Congreso Internacional Ciencais Administrativas y Económicas" />
+	<meta property="og:title" content="IV Congreso Internacional Ciencias Administrativas y Económicas" />
 	<meta property="og:description" content="IV Congreso Internacional Ciencias Administrativas y Económicas" />
 	<meta property="og:url" content="https://www.congresoute.org/" />
 	<meta property="og:site_name" content="FACULTAD CIENCIAS ADMINISTRATIVAS Y ECONÓMICAS" />
