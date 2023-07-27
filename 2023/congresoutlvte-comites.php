088b22acf093ac0378c8f7e71cf911aa619d059f
Stain Francis <educaysoft@gmail.com>
2023-07-26 23:53:22 -0400

    mejoras

--- 2023/congresoutlvte-comites.php
+++ 2023/congresoutlvte-comites.php
@@ -2,28 +2,72 @@
 <div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 100%; display: inline-block; ">
 
 <div style="float:left; width: 20%; font-size: 0.5wv;">
-  <header>
+<div> 
+ <header>
     <p style="margin-bottom: 10px; margin-top:10px;"><b>AUTORIDADES</b></p>
   </header>
   <main>
 
   <p><span class="nombre">Dr. Girald David Vernaza Arroyo. PhD.</span><br> 
-  Rector de la UTLVTE</p>
-
-  <p> <span class="nombre">Dra. Elizabeth Canchingre Bone. PhD.</span><br> 
-  Vicerrectora Académica de la UTLVTE<br>
-
-
+<i style="color: blue">PRESIDENTE DEL CONGRESO</i><br>  
+Rector de la UTLVTE</p>
 
   <p><span class="nombre">Dr. Manuel Quiñonez Cabeza. PhD.</span><br> 
+<i style="color: blue">PRIMER VICE-PRESIDENTE</i><br>  
   Vicerrector de Investigación y Postgrado</p>
+  
 
+<p> <span class="nombre">Dra. Elizabeth Canchingre Bone. PhD.</span><br> 
+<i style="color: blue">SEGUNDA VICE-PRESIDENTA</i><br>  
+  Vicerrectora Académica de la UTLVTE<br>
 
   <p><span class="nombre">Dra. Mariuxi Guijarro Cagua, PhD.</span><br> 
+<i style="color: blue">COORDINADORA GENERAL</i><br>  
   Decana de la FACAE-UTLVTE</p>
 
 
   </main>
+</div>
+<div>
+  <header>
+    <p  style="margin-bottom: 10px; margin-top:10px;" ><b>COORDINADOR GENERAL DEL EVENTO</b></p>
+  </header>
+  <main>
+
+  <p><span class="nombre">Dra. Mariuxi Guijarro Cagua, PhD.</span><br> 
+  <span class="nombre">Ing. Jorge Enrrique Salazar Sánchez, MBA.</span></p> 
+
+
+</main>
+</div>
+
+<div>
+  <header>
+    <p  style="margin-bottom: 10px; margin-top:10px;" ><b>ASESOR RELATOR ACADÉMICO</b></p>
+  </header>
+  <main>
+  <p><span class="nombre">Dr. Marco Raúl Quintero González, Mgs.</span></p> 
+
+</main>
+</div>
+
+<div>
+  <header>
+    <p  style="margin-bottom: 10px; margin-top:10px;" ><b>RESPONSABLES DE LA LOGÍSTICA</b></p>
+  </header>
+  <main>
+
+  <p><span class="nombre">Ing. María Fernanda Mosquera Torres, Mgs.</span><br> 
+  <span class="nombre">Ing. Mayra Alejandra Escandón Franco, Mgs.</span><br> 
+  <span class="nombre">Ing. Romina Tanagra Oyarvide Ibarra, Mgs.</span><br> 
+  <span class="nombre">Ing. Elaine Yanin Tenorio Canchingre, Mgs.</span><br> 
+  <span class="nombre">Ing. Ana Lucia Quintana Lovato, Mgs.</span><br> 
+  <span class="nombre">Ing. Jazmina Andrea Reina Zambrano, Mgs.</span></p> 
+
+
+</main>
+</div>
+
 </div>
 
 <div style="float:left; width: 20%;">
@@ -33,96 +77,233 @@
   </header>
   <main>
 
-   <p  class="badge bg-primary text-wrap" >Nacionales</p>
- <p class="nombre">  Dr. Alfonso Murgueitio Montenegro, PhD.<br><span class="text-primary">(Ecuador)</span></p>
-  <p class="nombre">  Dra. Elizabeth Gobea Robinsón, PhD.<br><span class="text-primary">(Ecuador)</span></p>
-   <p class="nombre"> Dr. Félix Rosales Cortes, PhD.<br><span class="text-primary">(Ecuador)</span></p>
-  <p class="nombre">Dr. Pablo Vilela Mera, PhD.<br><span class="text-primary">(Ecuador)</span></p>
-  <p class="nombre">Dra. Marisol Alava Cedeño, Mgs.<br><span class="text-primary">(Ecuador)</span></p>
+
+  <p><span class="nombre">Dr. José Armando Estrada Hernández, PhD.</span><br> 
+<i style="color: blue">Presidente del Comité Científico Internacional</i><br>  
+  (Universidad de Oriente, Santiago de Cuba)</p>
+
+  
+
+   <p  class="badge bg-primary text-wrap" >Miembros</p>
+ <p class="nombre">  Dr. Yorbet Montes de Oca Rojas, PhD.<br><span class="text-primary">(Universidad de Zulia, Venezuela)</span></p>
+  <p class="nombre">  Dr. Rosario León Robaina, PhD.<br><span class="text-primary">(Universidad del Oriente, Santiago de Cuba)</span></p>
+   <p class="nombre"> Dr. Graciela María Castellanos Pallerois, PhD.<br><span class="text-primary">(Universida del Oriente, Santiago de Cuba)</span></p>
+  <p class="nombre">Dr. Yahilina Silveira Perez, PhD.<br><span class="text-primary">(Universidad de Sucre, Colombia)</span></p>
+  <p class="nombre">Dr. Rubén Guillermo Pelliecer Durán, PhD.<br><span class="text-primary">(Universidad de Oriente, Santiago de Cuba)</span></p>
+
+  <p class="nombre">Dr. María Helena López Hernández, PhD.<br><span class="text-primary">(Universidad de Guadalajara, México)</span></p>
 
 
-    <p  class="badge bg-danger text-wrap" >Internacionales</p>
-<p class="nombre">Dr. Hoover Wait Ruiz Rengifo, PhD.</br>
- Dr. José Armando Estrada Hernández, PhD.<br>
-  Dr. Yahilina Silveira Pérez, PhD.<br>
-  Dr. Rosario León Robaina, PhD.<br>
-  Dr. Michael Tamayo, PhD.<br>
-  Dr. Angel Lemoine, PhD.<br>
-  Dr. María Helena López Hernández, PhD.<br>
-  Dr. Luis Alberto Pérez Llody, PhD.<br>
-  <p class="nombre">Dr. Tibisay Limus, PhD.<br><span class="text-primary">(Venezuela)</span></p>
+  <p class="nombre">Dr. Frank Ángel Lemoine Quintero, PhD.<br><span class="text-primary">(Universidad Laica Eloy Alfaro de Manabí, Extensión Bahía, Nacionalidad Cubana)</span></p>
 
 
-  <p class="nombre">Dra. Yorberth Montes de Oca Rojas, PhD.<br><span class="text-primary">(Venezuela)</span></p><br>
+  <p class="nombre">Dr. Alfonso Murgueitio Montenegro, PhD.<br><span class="text-primary">(Universidad Técnica Luis Vargas Torres de Esmeraldas - Ecuador)</span></p>
+
+  <p class="nombre">Dr. Elizabeth Jackeline Govea Robinsón, PhD.<br><span class="text-primary">(Universidad Técnica Luis Vargas Torres de Esmeraldas - Ecuador)</span></p>
+
+  <p class="nombre">Dr. Olga jackeline Quiñonez Guagua, PhD.<br><span class="text-primary">(Universidad Técnica Luis Vargas Torres de Esmeraldas - Ecuador)</span></p>
+
+
+  <p class="nombre">Dr. Harold Paúl Oyarvide Ramiréz, PhD.<br><span class="text-primary">(Universidad Técnica Luis Vargas Torres de Esmeraldas - Ecuador)</span></p>
+
+  <p class="nombre">Dr. Félix Segundo Rosales Cortes, PhD.<br><span class="text-primary">(Universidad Técnica Luis Vargas Torres de Esmeraldas - Ecuador)</span></p>
+
 
   </main>
 </div>
 
 <div style="float:left; width: 20%; padding-right:10px;">
+<div>
   <header>
     <p  style="margin-bottom: 10px; margin-top:10px;" ><b>COMITÉ ORGANIZADOR</b></p>
   </header>
   <main>
 
-   <p  class="badge bg-primary text-wrap" > V Foro Internacional de Talento
+  <p><span class="nombre">Dr. Pablo Aníbal Minda Batallas, PhD..</span><br>
+ Director de Investigación UTLVTE </p> 
+
+  <p><span class="nombre">Dr. Harold Paúl Oyarvide Ramírez, PhD.</span><br>
+(UTLVTE)</p>
+
+  <p><span class="nombre">Dr. Félix Segundo Rosales Cortes, PhD.</span><br>
+(UTLVTE)</p>
+
+
+  <p><span class="nombre">Ing. Walter Edgar Saavedra Segura, Mgs.</span><br>
+(UTLVTE)</p>
+
+
+  <p><span class="nombre">Ing. Ana Lucía Quintana Lovato, Mgs.</span><br>
+(UTLVTE)</p>
+
+
+  <p><span class="nombre">Ing. Carlos Andrés Holguin Cedeño, Mgs.</span><br>
+(UTLVTE)</p>
+
+  <p><span class="nombre">Ing. María Fernanda Caicedo Nazareno, Mgs.</span><br>
+(UTLVTE)</p>
+</main>
+</div>
+
+<div>
+
+  <header>
+    <p  style="margin-bottom: 10px; margin-top:10px;" ><b>FORO INTERNACIONAL</b></p>
+  </header>
+  <main>
+
+   <p  class="badge bg-primary text-wrap" > Tema: Talento
 Humano</p>
 
-  <p><span class="nombre">Ing. María Fernanda Mosquera , Mgs.</span><br> 
+  <p><span class="nombre">Ing. Jazmina Andrea Reina Zambrano, Mgs.</span><br> 
+  <span class="nombre">Ing. Norma Shirley Ayovi Realpe, Mgs.</span><br> 
+  <span class="nombre">Ing. Mercedes Magdalena Bustos Gámez, Mgs.</span><br> 
+  <span class="nombre">Ing. María Fernanda Mosquera Torres, Mgs.</span><br> 
+  <span class="nombre">Ing. Ana Lucia Quintana Lovato, Mgs.</span><br> 
+  <span class="nombre">Ing. Yimabel del Rocío Cortes Arce, Mgs.</span><br> 
+  <span class="nombre">Ing. Mayra Alejandra Escandón Franco, Mgs.</span><br> 
+  <span class="nombre">Ing. Jefferson Humberto Figueroa Moreno, Mgs.</span></p>
 
-  <span class="nombre">Ing. Norma Ayovi Realpe, Mgs.</span></p>
 
-
-   <p  class="badge bg-primary text-wrap" > II Foro Internacional Geopolítico
+   <p  class="badge bg-primary text-wrap" > Tema: Geopolítico
 Mundial</p>
-  <p><span class="nombre">Dr. Félix Rosales Cortes, PhD.</span><br> 
-  <span class="nombre"> Ing. Walter Saavedra Segura. Mgs.</span><br>
-  <span class="nombre"> Ing. Gorqui Estupiñán Vivas.</span></p>
-
-
-   <p  class="badge bg-primary text-wrap">III Coloquio Estudiantil: Semilleros de investigación</p>
-
-  <p><span class="nombre">Dr. Alfredo Plaza Castillo, PhD.</span><br> 
-  <span class="nombre"> Ing. Jorgeli Ayovi Caicedo, Mgs.</span><br>
-  <span class="nombre"> Estudiante,  Aron Estupiñan.</span></p>
-    <p  class="badge bg-primary text-wrap" >Comunicaciones</p>
-
-  <p><span class="nombre">Dr. Patricio Arce Minda,  Mgs.</span><br> 
-  <span class="nombre">Ing. Pascolin Guilcatoma Moreira, Mgs.</span><br>
-  <span class="nombre">Ing. Elaine Tenorio Canchingre, Mgs.</span><br>
-  <span class="nombre">Abg. José Cortez Altafulla.</span><br>
-  <span class="nombre">Ing. Cesar Godoy Rosero, Mgs.</span><br>
-  <span class="nombre">Ing. Ana Lucía Quintana Lovato, Msg.</span><br>
-  <span class="nombre">Ing. Mayra Escandón Franco, Mgs.</p>
-
-    <p  class="badge bg-primary text-wrap" >Moderadores</p>
-
-  <p> <span class="nombre">Ing. Luis Canchingre Bone, Mgs.</span><br>
-  <span class="nombre">Ing. Romina Oyarvide Ibarra, Mgs.</span><br>
-  <span class="nombre">Ing. Maria Fernanda Mosquera Torres, Mgs.</span><br>
-  <span class="nombre">Ing. Iván Endara Arguello, Mgs.</p>
-
-
- 
-
+  <p><span class="nombre">Dr. Félix Segundo Rosales Cortes, PhD.</span><br> 
+  <span class="nombre"> Ing. Walter Edgar Saavedra Segura. Mgs.</span><br>
+  <span class="nombre"> Ing. Gorqui Augusto Estupiñan Vivas.</span><br>
+  <span class="nombre"> Ing. Gustavo Roberto Castillo Ruano, Mgs.</span><br>
+  <span class="nombre"> Ing. José Jorge Cortez Altafuya, Mgs.</span><br>
+  <span class="nombre"> Ing. Pacha Marquez, Mgs.</span><br>
+  <span class="nombre"> Ing. Ingrid Tania Nazareno Veliz, Mgs.</span><br>
+  <span class="nombre"> Eco. Jorge Antonio Merchán Rumbea.</span></p>
   </main>
+</div>
 </div>
 
 <div style="float:left; width: 20%; padding-right:10px">
+
+<div>
   <header>
-    <p style="margin-bottom: 10px; margin-top:10px;" ><b>COMITÉ DE EDITORIAL DEL LIBRO</b></p>
+    <p style="margin-bottom: 10px; margin-top:10px;" ><b>COLOQUIO ESTUDIANTIL</b></p>
   </header>
 
   <main>
 
-  <p><span class="nombre">Dr. Harol Oyarvide Ramirez,PhD.</span><br>
-  <span class="nombre">Dr. José Armando Estrada Hernández, PhD.</span><br>
-  <span class="nombre">Ing. Raúl Camacho Marín, Mgs.</span></p>
+  <p><span class="nombre">Dr. Alfredo Plaza Castillo,PhD.</span><br>
+  <span class="nombre">Ing. Jesús Alfredo Gómez Ladines, Mgs.</span><br>
+  <span class="nombre">Ing. Walter Becerra Quiñonez, Mgs.</span><br>
+  <span class="nombre">Ing. Elaine Yanin Tenorio Canchingre, Mgs.</span></p>
 
+<p> <span class="nombre">Estudiante Cesar Augusto Rivadeneira Mendoza.</span><br>
+(CARRERA DE ADMINISTRACIÓN)</p>
+
+<p> <span class="nombre">Estudiante Anallely Mercedes Mora Robinson.</span><br>
+(CARRERA DE COMERCIO)</p>
+
+  </main>
+</div>
+
+
+
+
+<div>
+  <header>
+    <p  style="margin-bottom: 10px; margin-top:10px;" ><b>CORDINADORES EJES TEMÁTICOS</b></p>
+  </header>
+  <main>
+<p> <span class="nombre">Ing. Carlos Enrique Rivas Vallejo, Mgs.</span><br> 
+<i style="color: blue">(Responsabilidad social y empresarial.)</i></p>
+
+
+<p> <span class="nombre">Dr. Olga Jackeline Quiñonez Guagua, PhD.</span><br> 
+<i style="color: blue">(Calidad de la Gestión Innovadora y disruptiva de los negocios.)</i></p>
+
+<p> <span class="nombre">Ing. Carlos Andrés Holguin Cedeño, Msg.</span><br> 
+<i style="color: blue">(Tecnologías y su relación con el crecimiento y el desarrollo económico.)</i></p>
+
+
+<p> <span class="nombre">Ing. Jorgeli Ayoví Caicedo, Mgs.</span><br> 
+<i style="color: blue">(Economía del siglo XXI, economía circular y en colores.)</i></p>
+
+
+<p> <span class="nombre">Ing. Luis Aldimir Canchingre Bone, Mgs.</span><br> 
+<i style="color: blue">(Rol de los escenarios actuales de la empresa pública y privada.)</i></p>
+
+<p> <span class="nombre">Ing. Romina Tanagra Oyarvide Ibarra, Mgs.</span><br> 
+<i style="color: blue">(Liderazgo, comunicación interna y cultura organizacional.)</i></p>
+
+
+</main>
+</div>
+
+<div>
+  <header>
+    <p style="margin-bottom: 10px; margin-top:10px;" ><b>RESPONSABLE DE LA PUBLICACIÓN</b></p>
+  </header>
+
+  <main>
+  <p><span class="nombre">Dr. Pablo Aníbal Minda Batallas, PhD.</span><br>
+ Director de Investigación UTLVTE </p> 
+
+
+  </main>
+</div>
+
+
+</div>
+<div style="float:left; width: 20%;">
+<div>
+  <header>
+    <p  style="margin-bottom: 10px; margin-top:10px;" ><b>COMISIÓN ACADÉMICA</b></p>
+  </header>
+  <main>
+<p> <span class="nombre">Dr. Féliz Segundo Rosales Cortes, PhD.</span><br> 
+<i style="color: blue">COORDINADOR ACADÉMICO</i></p>  
+<p><span class="nombre">Dr. Julio Alfonso Murgueitto Montenegro. PhD.</span></br> 
+<span class="nombre">Dr. Pablo Domingo Vilela Mera, PhD.</span></br> 
+<span class="nombre">Dra. Marisol del Rocio Alava Cedeñó, Mgs.</span></br> 
+<span class="nombre">Dr. Gustavo Darío Robles Quiñónez</span></br> 
+<span class="nombre">Ing. Fanny Graciela Egas Moreno, Mgs</span></br> 
+<span class="nombre">Ing. Alex Paul Alencastro Guerrero, Mgs.</span></p> 
+
+
+</main>
+</div>
+<div>
+  <header>
+    <p style="margin-bottom: 10px; margin-top:10px;" ><b>MODERADORES</b></p>
+  </header>
+  <main>
+
+  <span class="nombre">Ing. Luis Aldimir Canchingre Bone, Mgs.</span><br> 
+  <span class="nombre">Ing. Romina Tamagra Oyarvide Ibarra, Mgs.</span><br> 
+  <span class="nombre">Ing. María Fernanda Mosquera Torres, Mgs.</span><br> 
+  <span class="nombre">Ing. Iván Octavio Endara Arguello, Mgs.</span><br> 
+  <span class="nombre">Ing. Mayra Alejandra Escandón Franco, Mgs.</span><br> 
+  <span class="nombre">Ing. Alex Paul Alencastro Guerrero, Mgs.</span><br> 
    
 
   </main>
 </div>
-<div style="float:left; width: 20%;">
+<div>
+  <header>
+    <p  style="margin-bottom: 10px; margin-top:10px;" ><b>COMISIÓN DE COMUNICACIONES</b></p>
+  </header>
+  <main>
+<p><span class="nombre">Ing. Elaine Yanin Tenorio Canchingres, MsC.</span></br> 
+<span class="nombre">Ing. Ana Lucía Quintana Lovato, Mgs.</span></br> 
+<span class="nombre">Ing. Romina Tanagra Oyarvide Ibarra, Mgs.</span></br> 
+<span class="nombre">Ing. María Fernanda Mosquera Torres, Mgs.</span></br> 
+<span class="nombre">Ing. Johanna Alejandra Custode Quiñonez, Mgs</span></br> 
+<span class="nombre">Ing. Yimabel del Rocio Cortes Arce, Mgs.</span></p> 
+
+
+</main>
+</div>
+
+
+
+
+<div>
   <header>
     <p style="margin-bottom: 10px; margin-top:10px;" ><b>COMITÉ TÉCNICO</b></p>
   </header>
@@ -133,6 +314,7 @@ Mundial</p>
    
 
   </main>
+</div>
 </div>
 
 
--- 2023/congresoutlvte-ejestematicos.php
+++ 2023/congresoutlvte-ejestematicos.php
@@ -83,7 +83,7 @@
  <div class="my-3 p-3 bg-body rounded shadow-sm">
 <ol id="lista5">
     <li> Responsabilidad social empresarial, economía circular, economía de colores y procesos regenerativos.<br><b>Coordinador:</b> Ing. Carlos Rivas Vallejo, Mgs.</li>
-    <li>Calidad de la Gestión Innovadora y descriptiva de los negocios.<br><b>Coordinadora:</b> Dra. Olga Quiñonez Cagua, PhD.
+    <li>Calidad de la Gestión Innovadora y descriptiva de los negocios.<br><b>Coordinadora:</b> Dra. Olga Quiñonez Guagua, PhD.
   </li>
     <li>Tecnología y su relación con el crecimiento y el desarrollo económico.<br><b>Coordinador:</b> Ing. Cesar Godoy Rosero, Mgs. </li>
     <li>Economía del siglo XXI economía circular y en colores.<br><b>Coordinador:</b> Jorgeli Ayovi Caicedo, Mgs.</li> 
